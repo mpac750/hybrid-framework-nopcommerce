69fbf2edd1484c9a8fa7b9b24fa88336047e5797
nguye <nguyenphat7509@gmail.com>
2024-05-31 17:34:56 +0700

    Add new project

---
+++ src/javaTester/javaBasic/Topic_01_Data_Type.java
@@ -0,0 +1,8 @@
+package javaTester.javaBasic;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_01_Data_Type {
+    @BeforeClass
+}
