4f2dd325d6b928d0f2f307007ed9358e68b29496
nguye <nguyenphat7509@gmail.com>
2024-06-05 17:24:10 +0700

    Add BasePage.java

--- src/actions/commons/BasePage.java
+++ src/actions/commons/BasePage.java
@@ -1,4 +1,98 @@
 package actions.commons;
 
-public class BasePage {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.Set;
+
+class BasePage {
+    WebDriver driver;
+
+    /*Web Browser*/
+    public Alert waitForAlertPresence(WebDriver driver) {
+        return new WebDriverWait(driver, Duration.ofSeconds(30)).until(ExpectedConditions.alertIsPresent());
+    }
+
+    public void openPageUrl(WebDriver driver, String pageUrl) {
+        driver.get(pageUrl);
+    }
+
+    public String getPageTitle(WebDriver driver) {
+        return driver.getTitle();
+    }
+
+    public String getCurrentPageUrl(WebDriver driver) {
+        return driver.getCurrentUrl();
+    }
+
+    public String getPageSourceCode(WebDriver driver) {
+        return driver.getPageSource();
+    }
+
+    public void backToPage(WebDriver driver) {
+        driver.navigate().back();
+    }
+
+    public void forwardToPage(WebDriver driver) {
+        driver.navigate().forward();
+    }
+
+    public void refreshCurrentPage(WebDriver driver) {
+        driver.navigate().refresh();
+    }
+
+    public void acceptToAlert(WebDriver driver) {
+        waitForAlertPresence(driver).accept();
+
+    }
+
+    public void cancelToAlert(WebDriver driver) {
+        waitForAlertPresence(driver).dismiss();
+    }
+
+    public String getTextInAlert(WebDriver driver) {
+        return waitForAlertPresence(driver).getText();
+    }
+
+    public void sendKeyToAlert(WebDriver driver, String keyToSend) {
+        driver.switchTo().alert().sendKeys(keyToSend);
+    }
+
+    public void switchToWindowByID(WebDriver driver,String expectedID) {
+        Set<String> allIDs = driver.getWindowHandles();
+        for (String id : allIDs) {
+            if (!id.equals(expectedID)) {
+                driver.switchTo().window(id);
+                break;
+            }
+        }
+    }
+
+    public void switchToWindowByTitle(WebDriver driver, String expectedTitle) {
+        Set<String> allIDs = driver.getWindowHandles();
+        for (String id : allIDs) {
+            driver.switchTo().window(id);
+            String actualTitle = driver.getTitle();
+            if (actualTitle.equals(expectedTitle)) {
+                break;
+            }
+        }
+
+    }
+
+    public void closeAllWindowsWithoutParent(WebDriver driver, String parentID) {
+        Set<String> allIDs = driver.getWindowHandles();
+        for (String id : allIDs) {
+            if (!id.equals(parentID)) {
+                driver.switchTo().window(id);
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentID);
+    }
+
 }
